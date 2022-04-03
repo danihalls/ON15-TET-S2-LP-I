3368efa55f76eadb0097372ad05fcbb601cefe26
Daniele <daniele.maria.silvadm@gmail.com>
2022-04-03 11:58:01 -0300

    subindo exercicio minecraft

---
+++ minecraft.js
@@ -0,0 +1,32 @@
+//Exercício 7 minecraft de Daniele Silva
+
+//script abaixo:
+
+1 -executar 6 vezes { 
+
+    plantar sementes
+    mover para frente
+
+}
+
+2 - virar a direita 
+
+3 - { executar 2 vezes 
+
+    mover para frente
+
+
+}
+
+4 - virar a direita
+
+5 - mover para frente
+
+6 - executar 5 vezes { 
+
+    plantar sementes
+    mover para frente
+
+}
+
+7 - plantar semente
